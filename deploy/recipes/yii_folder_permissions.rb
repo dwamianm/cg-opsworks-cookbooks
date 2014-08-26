8790a7338f7b07e89518432964c84faeb29bd018
dwamianm <mcleish@cornersix.com>
2014-08-26 11:07:00 -0500

    Update yii_folder_permissions.rb

--- deploy/recipes/yii_folder_permissions.rb
+++ deploy/recipes/yii_folder_permissions.rb
@@ -2,9 +2,10 @@ node[:deploy].each do |application, deploy|
 
   assets_folder = "#{deploy[:deploy_to]}/current/public_html/assets"
   runtime_folder = "#{deploy[:deploy_to]}/current/protected/runtime"
+  uploads_folder = "#{deploy[:deploy_to]}/current/public_html/uploads"
 
   execute "chmod -Rf 777 #{assets_folder} #{runtime_folder}" do
-    command "chmod -Rf 777 #{assets_folder} #{runtime_folder}"
+    command "chmod -Rf 777 #{assets_folder} #{runtime_folder} #{uploads_folder}"
   end
 
 end
