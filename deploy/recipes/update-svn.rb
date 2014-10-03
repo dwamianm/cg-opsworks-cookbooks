9db46ee34a4bf4e8a92e904c7ba0569e24099842
dwamianm <dmcleish@gmail.com>
2014-10-03 09:47:39 -0500

    update recipe to point to deploy

--- deploy/recipes/update-svn.rb
+++ deploy/recipes/update-svn.rb
@@ -1,7 +1,11 @@
 node[:deploy].each do |application, deploy|
 
+  application_folder = "#{deploy[:deploy_to]}/current"
+
   subversion "Update SVN" do
     repository deploy[:scm][:repository]
+    revision "HEAD"
+    destination "#{application_folder}"
     user deploy[:user]
     group deploy[:group]
     svn_username deploy[:scm][:user]
