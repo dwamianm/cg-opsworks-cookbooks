a5b93ceddde3fe22c36b76083a1aabe8a7dfc3a8
dwamianm <dwamianm@users.noreply.github.com>
2015-05-25 23:33:17 -0500

    Updated Git Update Action
    Update

--- deploy/recipes/git-update.rb
+++ deploy/recipes/git-update.rb
@@ -4,10 +4,8 @@ node[:deploy].each do |application, deploy|
 
   git "Update Git" do
     repository deploy[:git_repository]
-    revision deploy[:git_revision]
     destination "#{application_folder}"
     action :sync
-    ssh_wrapper "ssh -i deploy[:scm][:ssh_key]"
   end
 
 end
